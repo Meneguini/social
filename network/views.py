52cda2f0efb774bec34ed4dff972c7dc9879b5a9
meneguini <mariana.meneguini@gmail.com>
2020-12-14 10:26:42 -0500

    cleaning login and register views' code

--- network/views.py
+++ network/views.py
@@ -43,7 +43,6 @@ def like(request, status):
         new_like.save()
         post.likes_number = post.likes_number + 1
         post.save()
-
         return JsonResponse({
             "msg": "liked",
             "likes_number": post.likes_number
@@ -54,7 +53,6 @@ def like(request, status):
     deleted_like.delete()
     post.likes_number = post.likes_number - 1
     post.save()
-
     return JsonResponse({
         "msg": "unliked",
         "likes_number": post.likes_number
@@ -263,8 +261,6 @@ def posts(request, postbox):
         "previous_page": previous_page,
         "start": start,
         "user_logged": user_logged,
-        # "user_logged": request.user.username,
-        # "likes": [like.serialize() for like in likes]
         "likes": likes
     }, safe=False)
 
@@ -288,52 +284,58 @@ def index(request):
 
 
 def login_view(request):
-
-    if request.method == "POST":
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "network/login.html", {
-                "message": "Invalid username and/or password."
-            })
-    else:
+    if request.method != "POST":
         return render(request, "network/login.html")
 
+    # Attempt to sign user in
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
 
+    # Check if authentication was successful
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "network/login.html", {
+            "message": "Invalid username and/or password."
+        })
+    
+        
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
 
 def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
-
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "network/register.html", {
-                "message": "Passwords must match."
-            })
-
-        # Attempt to create new user
-        try:
-            user = User.objects.create_user(username, email, password)
-            user.save()
-        except IntegrityError:
-            return render(request, "network/register.html", {
-                "message": "Username already taken."
-            })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
-    else:
+    if request.method != "POST":
         return render(request, "network/register.html")
+
+    username = request.POST["username"]
+    email = request.POST["email"]
+
+    # Ensure password matches confirmation
+    password = request.POST["password"]
+    confirmation = request.POST["confirmation"]
+    if password != confirmation:
+        return render(request, "network/register.html", {
+            "message": "Passwords must match."
+        })
+    # Ensure password have at least 6 characters
+    if len(password) < 6:
+        return render(request, "network/register.html", {
+            "message": "Passwords must have at least 6 characters."
+        })
+
+    # Attempt to create new user
+    try:
+        user = User.objects.create_user(username, email, password)
+        user.save()
+    except IntegrityError:
+        return render(request, "network/register.html", {
+            "message": "Username already taken."
+        })
+    login(request, user)
+    return HttpResponseRedirect(reverse("index"))
+    
+
